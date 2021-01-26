89c1e923c8afa68504f85354c8bd084d2878c524
amanchauhan112 <48175246+amanchauhan112@users.noreply.github.com>
2021-01-27 01:38:07 +0530

    Delete youtube.js

--- youtube.js
+++
@@ -1,8 +0,0 @@
-module.exports ={
-    name:'youtube',
-    description:"this is youtube command",
-    execute(client,message,args){
-        message.channel.send('https://www.youtube.com');
-        // message.channel.send(`https://www.youtube.com+${variable}`)
-    }
-}
